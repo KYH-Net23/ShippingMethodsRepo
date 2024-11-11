8b70f1590a29564dcf7dac19a1649827610fe038
Mille Elfver <mille.elfver98@gmail.com>
2024-11-11 16:36:46 +0100

    Added new endpoint to ShippingController.cs for Transit Times calculation one postal code to another

--- ShippingProvider/Controllers/ShippingController.cs
+++ ShippingProvider/Controllers/ShippingController.cs
@@ -1,13 +1,14 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ShippingProvider.DTO.TransitTimeDTOs;
 using ShippingProvider.Services;
 
 namespace ShippingProvider.Controllers;
 
-[Route("api/[controller]")]
+[Route("[controller]")]
 [ApiController]
 public class ShippingController(IShippingService shippingService) : ControllerBase
 {
-    [HttpGet("NearestServicePointsByPostalCode")]
+    [HttpGet("ServicePoints")]
     public async Task<IActionResult> GetNearestServicePoints(
         [FromQuery] string postalCode,
         [FromQuery] int numberOfServicePoints = 3)
@@ -34,4 +35,24 @@ public class ShippingController(IShippingService shippingService) : ControllerBa
             return StatusCode(500, new { error = "Internal Server Error", details = ex.Message });
         }
     }
+    
+    [HttpGet("TransitTimes")]
+    public async Task<IActionResult> GetTransitTimes([FromQuery] TransitTimeRequest request)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+
+        try
+        {
+            var response = await shippingService.GetTransitTimesAsync(request);
+            return Ok(response);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, new { error = "Internal Server Error", details = ex.Message });
+        }
+    }
+
 }
