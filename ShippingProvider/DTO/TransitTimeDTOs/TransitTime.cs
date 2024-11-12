c521bf502481078d1892da55fb16dd35cc74f446
Mille Elfver <mille.elfver98@gmail.com>
2024-11-12 13:57:38 +0100

    Removed fields from request and removed unused DTO

--- ShippingProvider/DTO/TransitTimeDTOs/TransitTime.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ShippingProvider.DTO.TransitTimeDTOs;
-
-public class TransitTime
-{
-    public string timeOfDeparture { get; set; }
-    public string timeOfArrival { get; set; }
-    public ServiceInformation serviceInformation { get; set; }
-}
