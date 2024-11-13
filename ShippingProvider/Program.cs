ace7006aecb7cf9c00a922b7dba233bed008ee85
Mille Elfver <mille.elfver98@gmail.com>
2024-11-13 07:47:31 +0100

    included scalar integration instead of swagger with .net 9.0

--- ShippingProvider/Program.cs
+++ ShippingProvider/Program.cs
@@ -1,7 +1,10 @@
 using Azure.Identity;
+using Scalar.AspNetCore;
 using ShippingProvider.Services;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddOpenApi();
 
 var vaultUri = new Uri($"{builder.Configuration["KeyVault"]!}");
 if (builder.Environment.IsDevelopment())
@@ -21,7 +24,7 @@ var secretKey = builder.Configuration["PostnordApiKey"];
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+// builder.Services.AddSwaggerGen();
 
 builder.Services.AddHttpClient();
 builder.Services.AddSingleton<IShippingService>(sp => 
@@ -41,8 +44,14 @@ var app = builder.Build();
 
 app.UseCors("AllowAll");
 
-app.UseSwagger();
-app.UseSwaggerUI();
+app.MapOpenApi();
+app.MapScalarApiReference(o =>
+{
+    o.WithTheme(ScalarTheme.Mars);
+});
+
+// app.UseSwagger();
+// app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
